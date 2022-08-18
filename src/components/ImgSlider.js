a29cfaacd66a7253fdff8f4d4ce17d5df7d294e0
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-08-18 23:09:51 +0600

    working on slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -14,7 +14,7 @@ function ImgSlider() {
         autoplay: true,
     };
     return (
-        <Slider {...settings}>
+        <Carousel {...settings}>
             <div>
                 <h1>1</h1>
             </div>
@@ -30,8 +30,28 @@ function ImgSlider() {
             <div>
                 <h1>5</h1>
             </div>
-        </Slider>
+        </Carousel>
     )
-}
+};
+
+
+// CSS styling
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+
+    & > button {
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+
+        &:hover {
+            opacity: 1;
+            transition: opacity 0.2s ease 0s;
+        }
+
+    }
+`;
+
 
 export default ImgSlider
