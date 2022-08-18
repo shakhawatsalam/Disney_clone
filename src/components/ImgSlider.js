88c7f6766317469d376a7a7279c2ae75e5ef90f0
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-08-18 23:29:37 +0600

    working on styling of slier

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -15,21 +15,26 @@ function ImgSlider() {
     };
     return (
         <Carousel {...settings}>
-            <div>
-                <h1>1</h1>
-            </div>
-            <div>
-                <h1>2</h1>
-            </div>
-            <div>
-                <h1>3</h1>
-            </div>
-            <div>
-                <h1>4</h1>
-            </div>
-            <div>
-                <h1>5</h1>
-            </div>
+            <Wrap>
+                <a href="s">
+                    <img src="/images/slider-badging.jpg" alt="" />
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href="s">
+                    <img src="/images/slider-scale.jpg" alt="" />
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href="s">
+                    <img src="/images/slider-badag.jpg" alt="" />
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href="s">
+                    <img src="/images/slider-scales.jpg" alt="" />
+                </a>
+            </Wrap>
         </Carousel>
     )
 };
@@ -51,7 +56,37 @@ const Carousel = styled(Slider)`
         }
 
     }
-`;
+    ul li button {
+        &:before{
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
 
+    li.slick-active button:before{
+        color: white;
+    }
+    .slick-list {
+        overflow: initial;
+    }
+
+    .slick-prev {
+        left: -75px;
+    }
+    .slick-next {
+        right: -75px;
+    }
+`;
+const Wrap = styled.div`
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+
+    a{
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    }
+`;
 
 export default ImgSlider
