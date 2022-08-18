8666726e383d2893e9f0f8cc1a020a9b0468db5e
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-08-19 00:02:08 +0600

    workin on viewer

---
+++ src/components/Viewers.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Viewers() {
+  return (
+      <Container>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+    </Container>
+  )
+};
+
+
+// CSS Styling
+
+const Container = styled.div`
+    margin-top: 30px;
+    padding: 30px 0px 26px;
+    display: grid;
+    grid-gap: 25px;
+    gap: 25px;
+    grid-template-columns: repeat(5, minmax(0,1fr));
+
+`;
+
+export default Viewers
