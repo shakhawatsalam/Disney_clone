f3391d8397c904e8c23f9c18a57532345325f941
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-08-20 17:47:43 +0600

    working in viewer.js

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -64,10 +64,10 @@ const Wrap = styled.div`
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     cursor: pointer;
-    overflow: hidden;
-    position: relative;
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-    border: 3px solid rgba(249, 249, 249, 0.1);
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
 
     img {
     inset: 0px;
@@ -80,6 +80,14 @@ const Wrap = styled.div`
     width: 100%;
     z-index: 1;
     top: 0;
+  }
+  
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    
   }
 `;
 
